49619a64de4964e4751ffc08f3d2ea37ae24a7c0
Mario Gurmesevski <gurmesevskimario@gmail.com>
2025-07-18 12:08:38 +0200

    added dynamic title for website

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,18 +10,32 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Mario Gurmeshevski - Home',
+      },
     },
     {
       path: '/skills',
       name: 'skills',
       component: SkillsView,
+      meta: {
+        title: 'Mario Gurmeshevski - Skills',
+      },
     },
     {
       path: '/contact',
       name: 'contact',
       component: ContactView,
+      meta: {
+        title: 'Mario Gurmeshevski - Contact',
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = (to.meta.title as string) || 'Mario Gurmeshevski'
+  next()
 })
 
 export default router
